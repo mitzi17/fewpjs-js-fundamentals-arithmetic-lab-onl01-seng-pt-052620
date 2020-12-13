756ef90dfab6c109dced2669d2b4151c1e760afc
Mitzi17 <m_estrada17@live.com>
2020-12-12 21:41:19 -0800

    commit tree f8a509b7a7ffc0ddcb9bb3ba353826b49ac25045

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+const random = Math.floor( Math.random() * 1000 ) + 1
+
+const mod = 29 % 5
+
+const max = Math.max(2, 4, 7, 10, 15, 20)
+
+
+
